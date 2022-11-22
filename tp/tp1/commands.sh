7a569ad3dd3c68d2682eee0b5b7fcc0d24397573
RABBAH Mahmoud Almostafa <mrabbah@gmail.com>
2022-11-22 15:36:15 +0100

    tp1

---
+++ tp/tp1/commands.sh
@@ -0,0 +1,8 @@
+# lancer l'application
+docker-compose up -d
+
+# accès à l'appli http://localhost:8083
+# login : admin@admin.com
+# pwd: Azerty$123
+
+# filtrer sur les images: image/jpeg,image/jpg,image/png
