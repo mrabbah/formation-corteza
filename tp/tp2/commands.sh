ef20304049bdfb0c6adef3d1dfd62090252756eb
RABBAH Mahmoud Almostafa <mrabbah@gmail.com>
2022-11-23 06:32:59 +0100

    init tp2

---
+++ tp/tp2/commands.sh
@@ -0,0 +1,15 @@
+# récupérer tp2
+git stash && git pull
+
+# Accéder à tp2
+cd tp2
+
+# lancer l'application
+docker-compose up -d
+
+# accès à l'appli http://localhost:8083
+# login : admin@admin.com
+# pwd: Azerty$123
+
+
+
