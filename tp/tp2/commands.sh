b2abfdb151ebc7a95d0337a7891d8690bc159830
RABBAH Mahmoud Almostafa <mrabbah@gmail.com>
2022-11-23 14:30:01 +0100

    new commands

--- tp/tp2/commands.sh
+++ tp/tp2/commands.sh
@@ -15,5 +15,40 @@ docker-compose up -d
 
 docker-compose logs -t --tail=100 -f server
 
+# lors de debug d'un workflow regarder la ligne qui contienne : workflow.workflow
 
+docker-compose logs -t --tail=100 -f server | grep workflow.workflow
 
+# API de test : https://jsonplaceholder.typicode.com/users
+
+var inputData;
+
+var inputData;
+try {
+        inputData = JSON.parse(input);
+} catch (e) {
+        throw new Error('could not parse input payload: ' + e);
+}
+
+if (!inputData.name) {
+        throw new Error('could not parse input payload, name missing');
+}
+
+return { name: inputData.name, email: inputData.id };
+
+var inputData;
+
+try {
+        inputData = JSON.parse(readRequestBody(input));
+} catch (e) {
+        throw new Error('could not parse input payload: ' + e);
+}
+
+if (!inputData.name) {
+        throw new Error('could not parse input payload, name missing');
+}
+
+return { name: inputData.name, email: inputData.id };
+# indiquer outside url pour redireger http://localhost:8083/compose/ns/namespace_handle/pages/page_id
+
+# pour accéder à une valuer recode.values.xyz
